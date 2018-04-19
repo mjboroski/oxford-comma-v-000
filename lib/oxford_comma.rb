737b3229e96162bfceba19eb9662262f5d5317b4
Michael Boroski <mjboroski@gmail.com>
2018-04-19 20:58:14 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-  return array.join(" and ")
+  if array.length==1
+    return array.join()
+  elsif array.length==2
+    return array.join(" and ")
+  else
+    while x<(array.length-1)
+      holder_string += (array[x]+", ")
+      x+=1
+    end
+    holder_string+= "and #{array.last}"
+  end  
 end
