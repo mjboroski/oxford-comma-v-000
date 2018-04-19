407f99e1ad4b4d52a930b50d80c80b31a01e47a3
Michael Boroski <mjboroski@gmail.com>
2018-04-19 20:59:59 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length==2
     return array.join(" and ")
   else
-    while x+1<(array.length)
+    while (x+1)<array.length
       holder_string += (array[x]+", ")
       x+=1
     end
